6237b6855e533dfacdbe8c260ee44dde97bc98eb
imadc1b <107376666+imadc1b@users.noreply.github.com>
2022-06-19 01:24:00 +0100

    Add files via upload

---
+++ freefire.py
@@ -0,0 +1,98 @@
+import time
+print("\033[91m")                  
+print("""
+ 
+   _____ _               _             
+  / ____| |             | |            
+ | |    | |__   ___  ___| | _____ _ __ 
+ | |    | '_ \ / _ \/ __| |/ / _ \ '__|
+ | |____| | | |  __/ (__|   <  __/ |   
+  \_____|_| |_|\___|\___|_|\_\___|_|   
+                                       
+                                        """)
+                                                                               
+
+                                                                                           
+
+                                                                      
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+print("\033[92m")
+
+print("""
+    ______ _____  ______ ______   ______ _____ _____  ______ 
+   |  ____|  __ \|  ____|  ____| |  ____|_   _|  __ \|  ____|
+   | |__  | |__) | |__  | |__    | |__    | | | |__) | |__   
+   |  __| |  _  /|  __| |  __|   |  __|   | | |  _  /|  __|  
+   | |    | | \ \| |____| |____  | |     _| |_| | \ \| |____ 
+   |_|    |_|  \_\______|______| |_|    |_____|_|  \_\______|""")
+                                                           
+print("\033")                                                           
+print("\033[97m     Coded By\033[96m IMAD C1B") 
+print("\033")    
+print("\033[97m       Created By\033[96m Imad C1B ")
+print("\033")
+print("\033[94m               With Great Pøwer Comes Great Responsibilites!")     
+print("\033")
+print("\033[91m [+]--------Its complexity is guessed by the complement--------[+]")      
+print("\033")
+print("\033[93m [+]---If you encounter a problem in the Combolist Contact me---[+]")
+print("\033[91m ")            
+
+print("\033[97m  Facebook\033[92m Drx Imad\033[94m")
+print("\033")
+
+print("\033")
+print("\033[97m                      WhatsApp :\033[91m +212 633978082")
+print("\033")
+print("\033[96m  [+] : Select your combo list to start the cracking process.")
+print("\033")               
+input("\033[96m  [+] : \033[97mCombo Name:")                                     
+print("\033")                 
+time.sleep(4)
+print("\033[92m   Crack FREE FIRE ACCOUNT done successfully....")      
+print("\033")
+time.sleep(2)
+print("\033[97m   Davisaron@Hotmail.com|11Dee57dha\033[91m[invalid]") 
+time.sleep(2)
+print("\033[97m   advoshitiz@gmail.com|1234DJO#@$Fa\033[91m[invalid]")   
+time.sleep(2)    
+print("\033[97m   JenyHom2020@Hotmail.com|dhr#$UHD312\033[92m[Valid]")
+time.sleep(2)
+print("\033[97m   srajnish@hotmail.com.com|112233655aa\033[92m[valid]")     
+time.sleep(2) 
+print("\033[97m   solkohli@gmail.com.com|KdhhA6266\033[92m[Valid]")   
+time.sleep(3)   
+print("\033[97m   shariqqamar@gmail.com|bastrao55771\033[91m[invalid]")
+time.sleep(3)
+print("\033[97m   kapuvala062@gmail.|com10203621\033[92m[valid]")
+time.sleep(2)
+print("\033[97m   cm.murali78@gmail.com|info##234\033[91m[invalid]")
+time.sleep(2)
+print("\033[97m    mig.academy.system@gmail.com|667773333\033[92m[valid]")
+time.sleep(2)
+print("\033[97m   anilnp33@gmail.com|a1n2i3l433\033[92m[valid]")
+time.sleep(2)
+print("\033[97m   Jhathaway029@gmail.com|Maxwell1!\033[91m [invaid]")
+time.sleep(2)
+print("\033[97m    modaine.beckford47@gmail.com|Beckford14\033[92m [valid]")
+time.sleep(2)
+print("\033[97m    mjchambers3rd@gmail.com|J04241959\033[91m [invalid]")
+time.sleep(2)
+print("\033[97m   hammer.kayla@gmail.com|Airvent1\033[92m [valid]")
+time.sleep(2)
+print("\033[97m   nickabbott70@gmail.com|Minstrels70\033[92m [valid]")
+time.sleep(2)
+print("\033[97m    mchantallemke14122002@gmail.com:Chantal4\033[92m [valid]")
+time.sleep(2)
+print("\033[97m    mduggancannon@yahoo.com|Dugganc7!\033[91m [invalid]")
+time.sleep(2)
+print("\033[97m   mbeniyedidyah@pm.me|Mitzv@h613\033[92m [valid]")
+time.sleep(2)
+print("\033[97m   aragongbsa@gmail.com|Aragon801!!\033[91m [invalid]")
+time.sleep(2)
+print("\033[97m   mtynusthree@gmail.com|Popcorn24!\033[92m [valid]")
+time.sleep(2)
+print("\033[97m   jocall123@gmail.com|Monster1\033[92m [valid]")
+
+
+                           
